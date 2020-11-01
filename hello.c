5da70060c5a8ee1521c8859ac002fc47a87e4530
David Regan <dpr333@nyu.edu>
2020-11-01 16:38:47 -0500

    Fixed Stupid Code Error

--- hello.c
+++ hello.c
@@ -2,7 +2,7 @@
 
 int main(int argc, char* argv[]) {
     printf("\n\nGosh Darned Hello travis ci\n\n");
-    printf("\n\nI managed to get a simple travis build to work - nothing more!\n")
+    printf("\n\nI managed to get a simple travis build to work - nothing more!\n");
 
     return 0;
 }
