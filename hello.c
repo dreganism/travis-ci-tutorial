06a457d0ab11c4ee70492e5e5b3f8dc04e47d484
David Regan <dpr333@nyu.edu>
2020-11-01 16:33:20 -0500

    Lame update

--- hello.c
+++ hello.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 int main(int argc, char* argv[]) {
-    printf("hello travis ci\n");
+    printf("\n\nGosh Darned Hello travis ci\n\n");
+    printf("\n\nI managed to get a simple travis build to work - nothing more!\n")
 
     return 0;
 }
